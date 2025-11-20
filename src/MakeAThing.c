797c0f04b703ea3e403038343ef3d1b56849af33
BlocksumoGuys <148206420+BlocksumoGuys@users.noreply.github.com>
2025-11-20 12:29:11 +0100

    Update MakeAThing.c

--- src/MakeAThing.c
+++ src/MakeAThing.c
@@ -31,7 +31,7 @@ int main(){
             1. Each C scripts need atleast a main function named 'main'
             2. all statements in C ends with a ';'.
             3. for handling singular characters, use '', for an array of characters, use "".
-            
+
         #include:
             #include <>: includes a library (header),
             <stdio.h>: basic input-output header,
